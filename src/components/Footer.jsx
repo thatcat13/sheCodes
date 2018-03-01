c581bb867c4d7a4a713455c0c361e4b761cd6f25
cj <thatcat13@gmail.com>
2018-03-01 10:34:46 -0800

    finish initial footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,6 +21,12 @@ function Footer(){
           bottom: 10%;
           transform: translate(50%, 50%);
         }
+        .copyright {
+          position: absolute;
+          right: 50%;
+          bottom: 5%;
+          transform: translate(50%, 50%);
+        }
         a {
           color: black;
           text-shadow: 3px 2px 3px grey;
@@ -34,6 +40,9 @@ function Footer(){
       `}</style>
       <div className="links">
         <Link to="/">Home </Link>  <Link to="/signup">Sign Up</Link>  <Link to="/about">About</Link>
+      </div>
+      <div className='copyright'>
+        <p>C 2018 sheCodes() All rights reserved.</p>
       </div>
     </div>
   );
